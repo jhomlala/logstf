e832544067febb3ca707975b4416db39f784cafb
Jakub <jhomlala@gmail.com>
2019-09-09 00:07:16 +0200

    Added observe player when searching by name

--- lib/widget/player_search_row_widget.dart
+++ lib/widget/player_search_row_widget.dart
@@ -1,38 +1,115 @@
 import 'package:flutter/material.dart';
 import 'package:logstf/bloc/logs_search_bloc.dart';
+import 'package:logstf/bloc/players_observed_bloc.dart';
+import 'package:logstf/model/player_observed.dart';
 import 'package:logstf/model/player_search_result.dart';
+import 'package:logstf/widget/logs_button.dart';
 
-class PlayerSearchRowWidget extends StatelessWidget {
+class PlayerSearchRowWidget extends StatefulWidget {
   final PlayerSearchResult playerSearchResult;
 
   const PlayerSearchRowWidget({Key key, this.playerSearchResult})
       : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return InkWell(child: Card(child:
-      Container(margin:EdgeInsets.all(5),child:Row(children: [
-       _getImage(playerSearchResult.avatarUrl),
-        Padding(padding: EdgeInsets.only(left:5)),
-        Container(width: MediaQuery.of(context).size.width*0.8,child: Text(playerSearchResult.playerNames.join(", "), maxLines: 5,))
-      ])),
-    ), onTap: (){
-      _onPlayerClicked(context);
+  State<StatefulWidget> createState() {
+    return _PlayerSearchRowWidgetState();
+  }
+}
+
+class _PlayerSearchRowWidgetState extends State<PlayerSearchRowWidget> {
+  bool isObserved = false;
+
+  @override
+  void initState() {
+    playersObservedBloc
+        .getPlayerObserved(widget.playerSearchResult.steamId)
+        .then((PlayerObserved playerObserved) {
+      if (mounted) {
+        setState(() {
+          isObserved = playerObserved != null;
+        });
+      }
     });
+    super.initState();
   }
 
-  _onPlayerClicked(BuildContext context) {
-    print("On player fclicked!!!");
-    logsSearchBloc.clearFilters();
-    logsSearchBloc.searchLogs(player: playerSearchResult.steamId);
-   Navigator.of(context).pop();
+  @override
+  void dispose() {
+    super.dispose();
   }
 
-  Widget _getImage(String url){
-    if (url != null){
-      return Image.network(playerSearchResult.avatarUrl);
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(10),
+      child: Container(
+          margin: EdgeInsets.all(5),
+          child: Column(children: [
+            Row(children: [
+              _getImage(widget.playerSearchResult.avatarUrl),
+              Padding(padding: EdgeInsets.only(left: 5)),
+              Container(
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  child: Text(
+                    widget.playerSearchResult.playerNames.join(", "),
+                    maxLines: 5,
+                  ))
+            ]),
+            Divider(),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: _getPlayerActionWidgets())
+          ])),
+    );
+  }
+
+  List<Widget> _getPlayerActionWidgets() {
+    List<Widget> list = List();
+    if (isObserved) {
+      list.add(Text("Player is observed"));
     } else {
-      return Image.asset("assets/tf2logo.png", width: 32,height: 32);
+      list.add(LogsButton(
+        text: "Observe player",
+        backgroundColor: Theme.of(context).primaryColor,
+        onPressed: () {
+          _onObservePlayerClicked(context);
+        },
+      ));
     }
+    list.add(Padding(
+      padding: EdgeInsets.only(left: 10),
+    ));
+    list.add(LogsButton(
+      text: "Search logs with player",
+      backgroundColor: Theme.of(context).primaryColor,
+      onPressed: () {
+        _onSearchLogsClicked(context);
+      },
+    ));
+    return list;
+  }
+
+  _onSearchLogsClicked(BuildContext context) {
+    logsSearchBloc.clearFilters();
+    logsSearchBloc.searchLogs(player: widget.playerSearchResult.steamId);
+    Navigator.of(context).pop();
+  }
+
+  Widget _getImage(String url) {
+    if (url != null) {
+      return Image.network(widget.playerSearchResult.avatarUrl);
+    } else {
+      return Image.asset("assets/tf2logo.png", width: 32, height: 32);
+    }
+  }
+
+  void _onObservePlayerClicked(BuildContext context) {
+    playersObservedBloc.addPlayerObserved(PlayerObserved(
+        name: widget.playerSearchResult.playerNames.first,
+        steamid64: widget.playerSearchResult.steamId));
+    setState(() {
+      isObserved = true;
+    });
   }
 }
