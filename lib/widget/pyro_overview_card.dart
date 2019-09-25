f7c46562981d9ea9ed9fb9e8a2a64b2c6892d577
Jakub <jhomlala@gmail.com>
2019-09-25 21:14:00 +0200

    Added class overview translations

--- lib/widget/pyro_overview_card.dart
+++ lib/widget/pyro_overview_card.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:logstf/model/class_stats.dart';
 import 'package:logstf/model/log.dart';
 import 'package:logstf/model/player.dart';
+import 'package:logstf/util/application_localization.dart';
 
 import 'base_overview_card.dart';
 import 'class_icon.dart';
@@ -26,6 +27,7 @@ class _PyroOverviewCardState extends BaseOverviewCardState<PyroOverviewCard> {
 
   @override
   Widget build(BuildContext context) {
+    var applicationLocalization = ApplicationLocalization.of(context);
     return Container(
         child: Expanded(
             child: ListView(children: [
@@ -39,23 +41,23 @@ class _PyroOverviewCardState extends BaseOverviewCardState<PyroOverviewCard> {
                     ClassIcon(playerClass: "pyro"),
                     Container(
                         child: Text(
-                          " Highlights",
+                          " ${applicationLocalization.getText("log_class_highlights")}",
                           style: TextStyle(fontSize: 20),
                         ))
                   ]),
-                  getStatRow("Time played: ", getTimePlayed(_classStats)),
+                  getStatRow("${applicationLocalization.getText("log_class_time_played")}: ", getTimePlayed(_classStats)),
                   Row(children: [
-                    getStatRow("Kills: ", _classStats.kills.toString()),
+                    getStatRow("${applicationLocalization.getText("log_kills")}: ", _classStats.kills.toString()),
                     getPositionRow(
-                        getPlayerKillsPosition(), "overall top kills", context)
+                        getPlayerKillsPosition(), "${applicationLocalization.getText("log_class_overall_top_kills")}", context)
                   ]),
                   Row(children: [
                     getStatRow(
-                      "Assists: ",
+                      "${applicationLocalization.getText("log_assists")}: ",
                       _classStats.assists.toString(),
                     ),
                     getPositionRow(getPlayerAssistsPosition(),
-                        "overall top assists", context)
+                        "${applicationLocalization.getText("log_class_overall_top_assists")}", context)
                   ]),
                   Row(children: [
                     getStatRow(
@@ -63,7 +65,7 @@ class _PyroOverviewCardState extends BaseOverviewCardState<PyroOverviewCard> {
                       getKillsPerDeath(_classStats).toStringAsFixed(1),
                     ),
                     getPositionRow(getPlayerKillsPerDeathPosition(),
-                        "overall top K/D", context)
+                        "${applicationLocalization.getText("log_class_overall_top_kpd")}", context)
                   ]),
                   Row(children: [
                     getStatRow(
@@ -71,15 +73,15 @@ class _PyroOverviewCardState extends BaseOverviewCardState<PyroOverviewCard> {
                       getKillsAndAssistsPerDeath(_classStats).toStringAsFixed(1),
                     ),
                     getPositionRow(
-                        getPlayerKAPDPosition(), "overall top KA/D", context)
+                        getPlayerKAPDPosition(), "${applicationLocalization.getText("log_class_overall_top_kapd")}", context)
                   ]),
                   Row(children: [
                     getStatRow(
-                      "Damage: ",
+                      "${applicationLocalization.getText("log_damage")}: ",
                       _classStats.dmg.toString(),
                     ),
                     getPositionRow(getPlayerDamagePosition(),
-                        "overall top damage", context)
+                        "${applicationLocalization.getText("log_class_overall_top_damage")}", context)
                   ]),
                   Row(children: [
                     getStatRow(
@@ -87,23 +89,23 @@ class _PyroOverviewCardState extends BaseOverviewCardState<PyroOverviewCard> {
                       getDamagePerMinute(_classStats).toStringAsFixed(0),
                     ),
                     getPositionRow(
-                        getPlayerDAPMPosition(), "overall top DA/M", context)
+                        getPlayerDAPMPosition(), "${applicationLocalization.getText("log_class_overall_top_dapm")}", context)
                   ]),
                   Divider(),
                   Row(children: [
                     getStatRow(
-                      "Medics killed: ",
+                      "${applicationLocalization.getText("log_class_medics_killed")}: ",
                       getMedicsKilled().toString(),
                     ),
                     getPositionRow(getPlayerMedicsPickedPosition(),
-                        "overall top medics killed", context)
+                        "${applicationLocalization.getText("log_class_overall_top_medics_killed")}", context)
                   ]),
                   Row(children: [
                     getStatRow(
-                      "Caps: ",
+                      "${applicationLocalization.getText("log_class_caps")}: ",
                       player.cpc.toString(),
                     ),
-                    getPositionRow(getPlayerCapPosition(), "overall top caps", context)
+                    getPositionRow(getPlayerCapPosition(), "${applicationLocalization.getText("log_class_overall_top_caps")}", context)
                   ]),
                 ],
               ))),
